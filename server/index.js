2932e47865f257d29f4879cdce206c79df6054a6
tosin-oni <tosin.oni@qlik.com>
2019-01-15 11:02:14 -0500

    testing scheduling

--- server/index.js
+++ server/index.js
@@ -59,6 +59,6 @@ app.get('*', function(req, res) {
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`))
 
 userController.createAdmin()
-schedule.scheduleJob({hour: 7}, () => {
+schedule.scheduleJob({hour: 12}, () => {
   emailService.sendDailyEmail()
 })
